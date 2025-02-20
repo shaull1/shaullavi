64370b6289e68d709fee245ce3ffbbc01035997d
shaull1 <shaullevy777@gmail.com>
2025-02-21 01:46:40 +0200

    Updated website - added new content

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 setTimeout(() => {
     document.getElementById('splitImageContainer').style.display = 'none';
 }, 4000); // 4000 מילישניות = 4 שניות
-    // אתחול EmailJS עם המזהה הפומבי שלך
 
-    
+
+
+
+    // אתחול EmailJS עם המזהה הפומבי שלך
     emailjs.init("894834523374");
 
     // מאזין לטופס כדי למנוע טעינת דף
@@ -20,19 +22,3 @@ setTimeout(() => {
             });
     });
 
-    const playButton = document.getElementById('playButton');
-
-    playButton.addEventListener('click', () => {
-        playButton.style.display = 'none'; // הסתר את כפתור ה-Play
-        loadGameScript();
-    });
-
-    function loadGameScript() {
-        const script = document.createElement('script');
-        script.src = 'game.js'; // הנתיב לקובץ ה-JavaScript של המשחק
-        script.onload = () => {
-            console.log('Game script loaded and executed.');
-            startGame(); // הפעלת המשחק לאחר טעינת הסקריפט
-        };
-        document.body.appendC
-    }
